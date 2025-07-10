ccba6adcf065b8fe4ee79eec54c322266e0a2dac
Killer2408 <74144958+Killer2408@users.noreply.github.com>
2025-07-10 02:14:31 +0200

    yippiee
    neuesteeeee standdeee

--- Scenes/Level/BetterPixel/script/player.gd
+++ Scenes/Level/BetterPixel/script/player.gd
@@ -5,10 +5,13 @@ const JUMP_VELOCITY = -300.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var camera_2d: Camera2D = $Camera2D
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 @export var bullet_time_duration := 0.0
 @export var time_stop_duration := 0.0
 @export var cooldown := 0.0
 var can_use_bullet_time := true
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 
@@ -43,6 +46,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 func activate_bullet_time():
 	if GameState.unlocked_skills["bullet_time"]:
@@ -64,6 +68,8 @@ func activate_time_stop():
 	await get_tree().create_timer(time_stop_duration, true).timeout
 	get_tree().paused = false
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 func _on_area_2d_body_entered(body: Node2D) -> void:
